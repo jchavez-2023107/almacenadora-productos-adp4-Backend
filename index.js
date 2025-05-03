807d7ff26f6087a7759a9a8a8379c46f459122b8
pbautista-2023158 <pbautista-2023158@kinal.edu.gt>
2025-05-03 17:58:40 -0600

    no se 2

---
+++ index.js
@@ -0,0 +1,44 @@
+"use strict"; // Habilita el modo estricto para mejorar la seguridad y evitar errores comunes
+
+import dotenv from "dotenv";
+dotenv.config(); // Cargar variables de entorno desde .env
+
+import { connectDB } from "./configs/mongo.js"; // Conexión a la base de datos
+
+//Agregar los datos por defectos en un futuro
+/* import { agregarUsuariosPorDefecto } from "./src/users/user.controller.js";
+import { agregarClientesPorDefecto } from "./src/clients/client.controller.js";*/
+import { createDefaultCategories } from "./src/category/category.default.js";
+import { createDefaultProducts } from "./src/products/products.default.js";
+import { createDefaultSupplier } from "./src/suppliers/suppliers.default.js";
+import { createDefaultUsers } from "./src/users/user.default.js";
+
+createDefaultProducts()
+createDefaultCategories()
+createDefaultSupplier()
+createDefaultUsers()
+
+import { initServer } from "./configs/app.js"; // Inicialización del servidor Express
+
+// Iniciar conexión a MongoDB
+ (async () => {
+  try {
+    await connectDB(); // Esperar a que la base de datos se conecte antes de levantar el servidor
+
+
+    // Espacio para llamar asíncronamente a los datos por defecto
+    /* await agregarUsuariosPorDefecto();
+    await agregarClientesPorDefecto();
+    await agregarProductosPorDefecto(); */
+
+
+    initServer(); // Inicializar el servidor Express solo si la BD está conectada
+  } catch (err) {
+    console.error(
+      "❌ Error crítico en la inicialización de la aplicación:",
+      err
+    );
+    process.exit(1); // Cerrar la aplicación si hay un error crítico
+  }
+})();
+
---
+++ src/products/products.model.js
@@ -0,0 +1,54 @@
+import {Schema, model} from "mongoose"
+
+const productSchema = Schema(
+    {
+        name: {
+            type: String,
+            maxLength: 50,
+            unique: true,
+            required: true
+        },
+        price: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            maxLength: 150,
+            required: true
+        },
+        stock: {
+            type: String,
+            required: true
+        },
+        dateDelivery: {
+            type: Date,
+            required: true
+        },
+        expirationDate: {
+            type: Date,
+            required: false
+        },
+        note: { //Para dejar una nota del motivo del porque se quiere agregar o quitar un producto
+            type: String,
+            maxLength: 250,
+            required: false
+        },
+        supplier: {
+            type: Schema.Types.ObjectId,
+            ref: 'Supplier',
+            required: true
+        },
+        category: {
+            type: Schema.Types.ObjectId,
+            ref: 'Category',
+            required: true
+        }
+    },
+    {
+        versionKey: false,
+        timestamps: false 
+    }
+)
+
+export default model ('Products', productSchema)
